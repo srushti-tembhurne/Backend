d7238144a20eaa57ab3fda818402dd51a0a05705
Bsujeet <bkumarsujeet@gmail.com>
2017-02-23 15:03:49 +0530

    changed method from post to get for logout
    Signed-off-by: Bsujeet <bkumarsujeet@gmail.com>

--- web/router/index.js
+++ web/router/index.js
@@ -6,7 +6,7 @@ const middleware = require('./../middleware')
 
 
 router.post('/api/login', api.login.login)
-router.post('/api/logout', api.login.logout)
+router.get('/api/logout', api.login.logout)
 router.post('/api/request', middleware.validate, api.userReq.handle)
 router.get('/api/request', middleware.validate, api.userReq.getRequest)
 
